bffd7964f9f34e4eb332856e91e9af85cc7443e2
Felix Fricke <felix@frickefamily.de>
2023-10-21 23:52:59 +0200

    add analysis of human move

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ import random
 import lmql
 import asyncio
 
+
 def does_ai_start():
     return random.randint(0, 1) == 0
 
 
-async def play():
-    with open('src/chess.lmql', 'r') as f:
-        query = f.read()
+async def main():
+    with open('src/make_move.lmql', 'r') as f:
+        query_make_move = f.read()
+    with open('src/analyse_human_move.lmql', 'r') as f:
+        query_analyse_human_move = f.read()
     board = chess.Board()
     next_move_is_ai = does_ai_start()
     color_ai = "white" if next_move_is_ai else "black"
@@ -20,7 +23,7 @@ async def play():
             print(board)
             if next_move_is_ai:
                 answer = await lmql.run(
-                    query,
+                    query_make_move,
                     color=color_ai,
                     fen=board.board_fen(),
                     legal_moves=set([board.san(uci) for uci in board.legal_moves]),
@@ -40,14 +43,17 @@ async def play():
             except chess.AmbiguousMoveError:
                 print("Ambiguous move")
                 continue
+            else:
+                answer = await lmql.run(
+                    query_analyse_human_move,
+                    color=color_ai,
+                    move=move,
+                    fen=board.board_fen(),
+                    model="openai/gpt-3.5-turbo-instruct")
+                print("AI feedback: " + answer.variables["ANALYSIS"])
             # print(board.board_fen())
             next_move_is_ai = not next_move_is_ai
-            
-
-
-def main():
-    asyncio.run(play())
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
